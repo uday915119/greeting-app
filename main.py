211bd0a23e6bacc90c986f00d1a13dc6054773b3
Uday pratap singh <pruday32@gmail.com>
2025-08-02 17:13:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def greet(name):
-    return f"Hello, {name}! Welcome to DevOps."
+def main():
+    try:
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
+
+        print(f"Addition: {a + b}")
+        print(f"Subtraction: {a - b}")
+        print(f"Multiplication: {a * b}")
+        print(f"Division: {a / b if b != 0 else 'Infinity'}")
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
-    user = input("Enter your name: ")
-    print(greet(user))
+    main()
